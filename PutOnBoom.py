4456c61b5eb6d3c52ae048ba08530a4f4c6f96d1
Mathieu Tournier <mathieutournier@gmail.com>
2018-05-31 12:24:15 +0200

    Put On UE Boom using Low Energy Message.

---
+++ PutOnBoom.py
@@ -0,0 +1,31 @@
+#!/bin/python
+# This script build and execute this : 
+#-------------------->YOUR_BOOM_MAC     -----------------------------> YOUR_BTMAC(no ":")+01 
+# gatttool -i hci0 -b XX:XX:XX:XX:XX:XX --char-write-req -a 0x0003 -n XXXXXXXXXXXX01
+# This command was found by Mathieu Tournier by reverse engineering BOOM2 bluetooth ATT traffic
+
+import subprocess
+import re
+
+print("Please Write BOOM MAC Adress :")
+commandRet = subprocess.check_output(['hciconfig'])
+
+print(commandRet)
+
+matches = re.findall("([0-9A-F]{2}:[0-9A-F]{2}:[0-9a-fA-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2})", commandRet)
+print(matches)
+btAdress = matches[0].translate(None,':')
+print(btAdress)
+
+if (matches) : 
+    print ('using ' + matches[0] + ' as BT Adress adaptor')
+    boomAddr = raw_input("BOOM Mac Adress ?")
+    boomAddr = re.findall("([0-9A-F]{2}:[0-9A-F]{2}:[0-9a-fA-F]{2}:[0-9A-F]{2}:[0-9A-F]{2}:[0-9A-F]{2})", boomAddr)
+    if (boomAddr) :
+        commandRet = subprocess.check_output(['gatttool', '-i', 'hci0', '-b', str(boomAddr[0]), '--char-write-req', '-a', '0x0003', '-n', btAdress + '01'])
+    else :
+        print("Invalid Bt Adress")
+    
+else :
+    print("No Adaptator Found")
+
